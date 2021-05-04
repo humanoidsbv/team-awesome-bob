aa21caad41a0f098a5b776bfdb8a68dd792db44c
Bob Verheij <77615482+BobVerheij@users.noreply.github.com>
2021-05-04 15:21:10 +0200

    Change name from NavBox to MenuNavigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import * as Styled from "./Header.styled";
-import NavBox from "./NavBox";
+import MenuNavigation from "./MenuNavigation";
 
 function Header() {
-    const [isOpen, setIsOpen] = useState(false);
-    const handleClick = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
 
   return (
     <>
-    <Styled.Header isOpen = {isOpen}>
-      <h1>team awesome</h1>
-      <div>
-      <img src="assets/plus-icon.svg" onClick = {handleClick}/>
-      </div>
-    </Styled.Header>
-    <NavBox isOpen={isOpen}></NavBox>
+      <Styled.Header isOpen={isOpen}>
+        <h1>team awesome</h1>
+        <div>
+          <img src="assets/plus-icon.svg" onClick={handleClick} />
+        </div>
+      </Styled.Header>
+      <MenuNavigation isOpen={isOpen}></MenuNavigation>
     </>
   );
 }
