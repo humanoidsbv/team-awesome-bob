7df7e6f85110985893d1b35141434965d9465d0c
Bob Verheij <77615482+BobVerheij@users.noreply.github.com>
2021-05-31 10:44:50 +0200

    Remove Use of RawTimeEntry

--- src/stores/ZustandStore.tsx
+++ src/stores/ZustandStore.tsx
@@ -5,8 +5,8 @@ import * as Type from "../types/types";
 import { TimeEntry } from "../types/types";
 
 type State = {
-  rawTimeEntries: Type.RawTimeEntry[];
-  setRawTimeEntries: (input: Type.RawTimeEntry[]) => void;
+  rawTimeEntries: Type.TimeEntry[];
+  setRawTimeEntries: (input: Type.TimeEntry[]) => void;
   setTeamMemberFilter: (input: Type.TeamMember) => void;
   setTeamMembers: (input: Type.TeamMember[]) => void;
   setTimeEntries: (input: TimeEntry[]) => void;
